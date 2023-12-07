4c25a7ec30ce8ad790fa70a57416d9b76a89d07e
sumon paul <sumpaul100@gmail.com>
2023-12-07 12:04:54 +0600

    top news designed

--- src/app/components/ui/latestNews/LatestNews.js
+++ src/app/components/ui/latestNews/LatestNews.js
@@ -1,24 +1,26 @@
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import React from 'react';
-
+import topImage from "@/assets/top-new-images.png"
+import Image from 'next/image';
 const LatestNews = () => {
     return (
         <Box className="my-5">
-            <Card sx={{ maxWidth: 345 }}>
+
+            <Card>
                 <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        alt="green iguana"
-                    />
+                    <CardMedia>
+                        <Image src={topImage} alt='top news' width="" height="" />
+                    </CardMedia>
                     <CardContent>
+                        <p className='py-2 px-4 bg-pink-800 max-w-[200px] text-center text-white rounded-lg font-medium'>Technology</p>
                         <Typography gutterBottom variant="h5" component="div">
-                            Lizard
+                            Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+                        </Typography>
+                        <Typography gutterBottom variant="span" className='text-sm my-2 block'>
+                            By Awlad Hossain - Mar 18 2023
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
                         </Typography>
                     </CardContent>
                 </CardActionArea>
--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,12 @@ const HomePage = () => {
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <h1>Latest News</h1>
+        <Grid item xs={8} className='mt-10'>
+          <div className='border-b-2 relative'>
+            <h1 className='border-b-4 border-red-500 max-w-[40%] -mb-[3px]'>
+              <span className='block ml-8 font-bold md:text-3xl'>National News</span>
+            </h1>
+          </div>
           <LatestNews></LatestNews>
         </Grid>
         <Grid item xs={4}>
